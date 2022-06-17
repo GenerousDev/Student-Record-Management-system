2d7ec78d8fd5ac05c397daefe6bec1ee832c668f
generousdev <omolaja.abubakar05@gmail.com>
2022-06-17 21:31:04 +0100

    complete controllers

---
+++ Controllers/BatchController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Student_Management_Platform.models;
+
+namespace Student_Management_Platform.Controllers
+{
+    public class BatchController : Controller
+    {
+        private StudentPortEntities db = new StudentPortEntities();
+
+        // GET: Batch
+        public ActionResult Index()
+        {
+            return View(db.batches.ToList());
+        }
+
+        // GET: Batch/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            batch batch = db.batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // GET: Batch/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Batch/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,batch1,year")] batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.batches.Add(batch);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(batch);
+        }
+
+        // GET: Batch/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            batch batch = db.batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // POST: Batch/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,batch1,year")] batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(batch).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(batch);
+        }
+
+        // GET: Batch/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            batch batch = db.batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // POST: Batch/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            batch batch = db.batches.Find(id);
+            db.batches.Remove(batch);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Student_Management_Platform.models;
+
+namespace Student_Management_Platform.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private StudentPortEntities db = new StudentPortEntities();
+
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View(db.courses.ToList());
+        }
+
+        // GET: Courses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            course course = db.courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Course1,Duration")] course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(course);
+        }
+
+        // GET: Courses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            course course = db.courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Course1,Duration")] course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(course).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            course course = db.courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            course course = db.courses.Find(id);
+            db.courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
